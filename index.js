0569099c2d605e068a7de83f325c6a7760704a82
Bree-M <brenda.wairimu@student.moringaschool.com>
2025-03-15 23:00:00 +0300

    first  update

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// code your solution here
+const saturdayFun = (activity = "roller-skate") => {
+    return `This Saturday, I want to ${activity}!`;
+};
+
+console.log(saturdayFun());
+console.log(saturdayFun("got to the beach"));
+
+
+const mondayWork = (activity = "go to the office") => {
+    return `On Monday, i got to ${activity}!`;
+
+};
+
+console.log(mondayWork());
+console.log(mondayWork("workout when i wake up"));
+
+const wrapAdjective = (flair = "*") => {
+    return (adjective = "special") => {
+        return `You are ${flair}${adjective}${flair}!`;
+
+    };
+};
+
+const encouragingPromptFunction = wrapAdjective("!!!");
+console.log(encouragingPromptFunction("amazing"));
+console.log(encouragingPromptFunction());
